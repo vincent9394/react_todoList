f9b9ee8acbf47e7ea994f0344506e3fee00e23e6
vincent <luk96515@gmail.com>
2021-11-04 19:51:50 +0800

    [react-todolist][vincent] refactor: reverse the todoitem order

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -5,8 +5,9 @@ const TodoGroup = () => {
   const todos = useSelector((state) => state.todoList);
 
   return (
-    <div>
-      {todos.map((todo) => (
+    <div>             
+      {todos.sort((num1,num2)=>num2.id- num1.id)
+      .map((todo) => (
         <TodoItem key={todo.id} todo={todo} />
       ))}
     </div>
